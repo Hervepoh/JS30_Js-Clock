312169304018f828ca9636f6fc9c92f474ea76bb
Ngando epoh herve <epohherve63@gmail.com>
2019-03-02 18:40:38 +0100

    behavior of the clock hand

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function setClock(){
+    const date=new Date();
+    const heures=date.getHours();
+    const minutes=date.getMinutes();
+    const secondes=date.getSeconds();
+    const secondesDegree=(secondes/60)*360;
+    const minutesDegree=(minutes/60)*360;
+    const heuresDegree=(heures/60)*360;
+
+    const aiguilleSecondes=document.getElementById('aiguille-secondes');
+    const aiguilleMinutes=document.getElementById('aiguille-minutes');
+    const aiguilleHeures=document.getElementById('aiguille-heures');
+    aiguilleSecondes.style.transform=`rotateZ(${secondesDegree}deg)`;
+    aiguilleMinutes.style.transform=`rotateZ(${minutesDegree}deg)`;
+    aiguilleHeures.style.transform=`rotateZ(${heuresDegree}deg)`;
+}
+
+setInterval(setClock,1000);
